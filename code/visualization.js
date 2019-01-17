c8a7a4437879387fbbd00795cb8b25b3dfaef6e9
Xandra Vos <vosxandra@gmail.com>
2019-01-17 13:18:30 +0100

    new europe

--- code/visualization.js
+++ code/visualization.js
@@ -14,19 +14,117 @@ window.onload = function() {
 
     // executes a function
     Promise.all(requests).then(function(response) {
-        var dataRen = response[1];
-        var countries = response[0];
-        console.log(dataRen)
-        var years = Object.keys(dataRen)
+        var allData = prepareDataLine(response);
+        var linechartList = allData[0];
+        var years = allData[1];
+        console.log(linechartList)
         console.log(years)
 
+        var countries = response[0];
         makeMap(countries)
 
+        makeLineChart(linechartList, years)
+
+        // makeDonutChart(year, country)
 
 }).catch(function(e){
     throw(e);
 });
 };
+
+function makeLineChart(data, years) {
+
+    var margin = {top: 50, right: 50, bottom: 50, left: 50}
+    var width = 700 - margin.left - margin.right
+    var height = 400 - margin.top - margin.bottom;
+
+    // The number of datapoints
+    // var n = 21;
+
+    var minValue = d3.min(data)
+    var maxValue = d3.max(data)
+    var minYear = d3.min(years)
+    var maxYear = d3.max(years)
+
+    var xScale = d3.scaleTime()
+                   .domain([minYear, maxYear])
+                   .range([0, width]);
+
+    var yScale = d3.scaleLinear()
+        .domain([minValue, maxValue])
+        .range([height, 0]);
+
+    var line = d3.line()
+    .x(function(d, i) {
+        return xScale(years[i])})
+    .y(function(d) { return yScale(d)})
+    xScale.domain(d3.extent(years, function(d) { return +d}));
+    yScale.domain(d3.extent(data, function(d) { return +d }));
+
+// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+// var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// 3. Call the x axis in a group tag
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+
+// 4. Call the y axis in a group tag
+svg.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+
+// 9. Append the path, bind the data, and call the line generator
+svg.append("path")
+    .datum(data) // 10. Binds data to the line
+    .attr("class", "line") // Assign a class for styling
+    .attr("d", line); // 11. Calls the line generator
+
+// 12. Appends a circle for each datapoint
+svg.selectAll(".dot")
+    .data(data)
+  .enter().append("circle") // Uses the enter().append() method
+    .attr("class", "dot") // Assign a class for styling
+    .attr("cx", function(d, i) { return xScale(i) })
+    .attr("cy", function(d) { return yScale(d.y) })
+    .attr("r", 5)
+      .on("mouseover", function(a, b, c) {
+  			console.log(a)
+        this.attr('class', 'focus')
+		})
+      .on("mouseout", function() {  })
+}
+
+function prepareDataLine(data) {
+    var country = "Belgium"
+    var dataEn = data[1];
+    var countries = data[0];
+
+    var years = Object.keys(dataEn);
+    console.log(years)
+    var countriesData = Object.values(dataEn)
+
+    linechartList = []
+    countriesData.forEach(function(d, i) {
+        linechartList.push(d[country]["Share of renewable energy in gross final energy consumption"])
+    });
+
+    console.log(linechartList)
+
+    return [linechartList, years];
+
+}
+
+function makeDonutChart() {
+
+}
 
 function makeMap(countries) {
     // Set tooltips
